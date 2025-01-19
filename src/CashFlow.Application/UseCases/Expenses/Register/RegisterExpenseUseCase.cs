e034ede289e57d74cd300cb85a5848653e2ef047
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-19 11:39:36 -0300

    feat: implement validation for expenses

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,7 +7,31 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        //TODO: Implement to validation
+        Validate(request);
+
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+
+        if (DateTime.Compare(request.Date, DateTime.UtcNow) > 0)
+        {
+            throw new ArgumentException("The date must be less than or equal to the current date");
+        }
+
+        if (Enum.IsDefined(typeof(PaymentType), request.PaymentType) == false)
+        {
+            throw new ArgumentException("The payment type is invalid");
+        }
     }
 }
