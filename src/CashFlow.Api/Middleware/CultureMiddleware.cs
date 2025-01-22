ad8c95513afdf1d8fbff4a537e4f73e536039c56
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-21 22:46:50 -0300

    fix: ensure culture-setting middleware works with Swagger

--- src/CashFlow.Api/Middleware/CultureMiddleware.cs
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -11,13 +11,18 @@ public class CultureMiddleware
     }
     public async Task Invoke(HttpContext context)
     {
-        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        var supportedLanguages = CultureInfo
+            .GetCultures(CultureTypes.AllCultures)
+            .ToList();
+
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
 
         var cultureInfo = new CultureInfo("en-US");
 
-        if (string.IsNullOrWhiteSpace(culture) == false)
+        if (string.IsNullOrWhiteSpace(requestedCulture) == false 
+            && supportedLanguages.Exists(language => language.Name.Equals(requestedCulture)))
         {
-            cultureInfo = new CultureInfo(culture);
+            cultureInfo = new CultureInfo(requestedCulture);
         }
 
         CultureInfo.CurrentCulture = cultureInfo;
