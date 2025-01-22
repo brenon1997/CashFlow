f33081e45799b01bca0a5378a503c090309cb82b
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-21 23:14:49 -0300

    test: add initial unit test for ExpenseValidator

---
+++ tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Test.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Test",
+            Description = "Test",
+            Amount = 10,
+            Date = DateTime.Now,
+            PaymentType = PaymentType.Cash
+        };
+        var validator = new RegisterExpenseValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
