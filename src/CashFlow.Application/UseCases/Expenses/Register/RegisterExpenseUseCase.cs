76749d7fa35bc122adc19870bddd787536fdad84
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-24 23:35:07 -0300

    feat: implement complete functionality for inserting expenses into the database

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,14 +1,32 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Respositories.Expenses;
 using CashFlow.Exception.ExceptionBase;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
-public class RegisterExpenseUseCase
+public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
+    private readonly IExpensesRepository _repository;
+
+    public RegisterExpenseUseCase(IExpensesRepository repository)
+    {
+        _repository = repository;
+    }
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
+
+        var entityExpense = new Expense
+        {
+            Title = request.Title,
+            Amount = request.Amount,
+            Date = request.Date,
+            PaymentType = (Domain.Enums.PaymentType)request.PaymentType
+        };
+
+        _repository.Add(entityExpense);
 
         return new ResponseRegisteredExpenseJson();
     }
