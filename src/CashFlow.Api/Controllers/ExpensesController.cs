90e60ecf92859f33ef594be23c8cf7d23c99435b
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-25 17:18:05 -0300

    feat: add endpoint get all expenses

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Application.UseCases.Expenses.Register;
+﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -20,5 +21,21 @@ public class ExpensesController : ControllerBase
         var response = await useCase.Execute(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<ResponseExpensesJson>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAll(
+        [FromServices] IGetAllExpensesUseCase useCase)
+    {
+        var response = await useCase.Execute();
+
+        if (response.Expenses.Count == 0)
+        {
+            return NoContent();
+        }
+
+        return Ok(response);
     }
 }
