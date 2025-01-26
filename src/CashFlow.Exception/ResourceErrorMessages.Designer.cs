8a1696de5e8bdf112295568f61e3de2774e7c65e
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-25 22:45:48 -0300

    feat: add expense not found error message to ResourceErrorMessages

--- src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -79,6 +79,15 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The expense was not found.
+        /// </summary>
+        public static string EXPENSE_NOT_FOUND {
+            get {
+                return ResourceManager.GetString("EXPENSE_NOT_FOUND", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The payment type is invalid..
         /// </summary>
         public static string PAYMENT_TYPE_INVALID {
