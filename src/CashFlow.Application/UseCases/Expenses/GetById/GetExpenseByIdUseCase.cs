a17a20500db9eceee9ad2112498c886f60a3c86f
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-25 22:59:07 -0300

    feat: add GetExpenseByIdUseCase implementation

---
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Respositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Expenses.GetById;
+public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
+{
+    private IExpensesRepository _expensesRepository;
+    private readonly IMapper _mapper;
+    public GetExpenseByIdUseCase(
+        IExpensesRepository expensesRepository,
+        IMapper mapper)
+    {
+        _expensesRepository = expensesRepository;
+        _mapper = mapper;
+    }
+    public async Task<ResponseExpenseJson> Execute(int id)
+    {
+        var expense = await _expensesRepository.GetById(id);
+
+        if (expense is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+
+        return _mapper.Map<ResponseExpenseJson>(expense);
+    }
+}
