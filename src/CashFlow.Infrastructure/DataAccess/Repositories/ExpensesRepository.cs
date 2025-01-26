e8feb6be47186227faa182ef038ead3ee23d8c39
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-25 22:18:18 -0300

    feat: add GetById method implementation in ExpensesRepository

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -20,4 +20,9 @@ internal class ExpensesRepository : IExpensesRepository
     {
         return await _dbContext.Expenses.AsNoTracking().ToListAsync();
     }
+
+    public async Task<Expense?> GetById(int id)
+    {
+        return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
+    }
 }
