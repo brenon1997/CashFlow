526d7db72ab5fdb70262063642bd3cd378b54445
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-26 00:04:28 -0300

    refactor: refactor ExceptionFilter to apply the Open/Closed Principle

--- src/CashFlow.Exception/ExceptionBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionBase/CashFlowException.cs
@@ -4,4 +4,6 @@ public abstract class CashFlowException : SystemException
     protected CashFlowException(string message) : base(message)
     {
     }
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
 }
--- src/CashFlow.Exception/ExceptionBase/NotFoundException.cs
+++ src/CashFlow.Exception/ExceptionBase/NotFoundException.cs
@@ -1,7 +1,16 @@
-﻿namespace CashFlow.Exception.ExceptionBase;
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionBase;
 public class NotFoundException : CashFlowException
 {
     public NotFoundException(string message) : base(message)
     {
+    }
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
     }
 }
