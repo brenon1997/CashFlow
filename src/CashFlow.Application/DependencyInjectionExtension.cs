b6f99e5eef9411343170ee20ecd0127a2d37a3df
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-27 11:29:15 -0300

    feat: implement DeleteExpenseUseCase for deleting expenses

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -2,6 +2,7 @@
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Application.UseCases.Expenses.Delete;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Application;
@@ -23,5 +24,6 @@ public static class DependencyInjectionExtension
         services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
         services.AddScoped<IGetAllExpensesUseCase, GetAllExpensesUseCase>();
         services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase>();
+        services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Domain.Respositories;
+using CashFlow.Domain.Respositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete;
+public class DeleteExpenseUseCase : IDeleteExpenseUseCase
+{
+    private readonly IExpensesWriteOnlyRepository _expensesWriteOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    public DeleteExpenseUseCase(
+        IExpensesWriteOnlyRepository expensesWriteOnlyRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _expensesWriteOnlyRepository = expensesWriteOnlyRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(long id)
+    {
+        var result = await _expensesWriteOnlyRepository.Delete(id);
+
+        if (result is false)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+
+        await _unitOfWork.Commit();
+    }
+}
