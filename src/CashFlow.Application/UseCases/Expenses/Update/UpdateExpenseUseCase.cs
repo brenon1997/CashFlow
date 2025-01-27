c249d5770ad98ca471147f5d2daaf9d74004701a
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-27 14:19:17 -0300

    feat: create UpdateExpenseUseCase with Execute and Validate method

---
+++ src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Respositories;
+using CashFlow.Domain.Respositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Update;
+public class UpdateExpenseUseCase : IUpdateExpenseUseCase
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IExpensesUpdateOnlyRepository _expensesUpdateOnlyRepository;
+
+    public UpdateExpenseUseCase(
+        IMapper mapper,
+        IUnitOfWork unitOfWork,
+        IExpensesUpdateOnlyRepository expensesUpdateOnlyRepository)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+        _expensesUpdateOnlyRepository = expensesUpdateOnlyRepository;
+    }
+
+    public async Task Execute(long id, RequestExpenseJson request)
+    {
+        Validate(request);
+
+        var expense = await _expensesUpdateOnlyRepository.GetById(id);
+
+        if (expense is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+
+        _mapper.Map(request, expense);
+
+        _expensesUpdateOnlyRepository.Update(expense);
+
+        await _unitOfWork.Commit();
+    }
+
+    private void Validate(RequestExpenseJson request)
+    {
+        var validator = new ExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid is false)
+        {
+            var errorMessages = result.Errors.Select(r => r.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
