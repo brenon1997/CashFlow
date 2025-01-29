99651568e012a1be0d8e27c159a9d6218776e8c2
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-29 10:42:36 -0300

    feat: add FilterByMonth method to IExpensesReadOnlyRepository and ExpensesReadOnlyRepository

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesReadOnlyRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesReadOnlyRepository.cs
@@ -20,4 +20,15 @@ internal class ExpensesReadOnlyRepository : IExpensesReadOnlyRepository
     {
         return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
     }
+
+    public async Task<List<Expense>> FilterByMonth(DateOnly date)
+    {
+        return await _dbContext
+            .Expenses
+            .AsNoTracking()
+            .Where(e => e.Date.Year == date.Year && e.Date.Month == date.Month)
+            .OrderBy(e => e.Date)
+            .OrderBy(e => e.Title)
+            .ToListAsync();
+    }
 }
