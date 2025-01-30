77f53b3cef619979d3e196a906871f8601107c07
Brenon Tavares <brenonperez1997@gmail.com>
2025-01-30 17:39:57 -0300

    feat: add GetPdf method in ReportController for generating PDF expense reports

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -21,6 +22,24 @@ public class ReportController : ControllerBase
             var fileName = $"ExpensesReport_{month:yyyyMM}.xlsx";
 
             return File(file, MediaTypeNames.Application.Octet, fileName);
+        }
+
+        return NoContent();
+    }
+
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf(
+        [FromServices] IGenerateExpensesReportPdfUseCase _useCase,
+        [FromHeader] DateOnly month)
+    {
+        byte[] file = await _useCase.Execute(month);
+
+        if (file.Length > 0)
+        {
+            var fileName = $"ExpensesReport_{month:yyyyMM}.pdf";
+            return File(file, MediaTypeNames.Application.Pdf, fileName);
         }
 
         return NoContent();
