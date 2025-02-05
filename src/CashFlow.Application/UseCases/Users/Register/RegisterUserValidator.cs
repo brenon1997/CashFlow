1d0a5eb26c49b2f5336c661fb0a6139019dd36f7
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-05 10:14:55 -0300

    feat: add RegisterUserValidator for user registration validation

---
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Users.Register;
+public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(ex => ex.Name)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.NAME_EMPTY);
+
+        RuleFor(ex => ex.Email)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.EMAIL_EMPTY);
+
+        RuleFor(ex => ex.Password)
+            .SetValidator(new PasswordValidator<RequestRegisterUserJson>());
+    }
+}
