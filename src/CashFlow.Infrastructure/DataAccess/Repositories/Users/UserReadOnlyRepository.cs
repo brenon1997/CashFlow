a58df2be6dd568a441b3028cade3695038e8b4f6
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-07 23:56:18 -0300

    feat: implement IUserReadOnlyRepository interface in UserReadOnlyRepository

--- src/CashFlow.Infrastructure/DataAccess/Repositories/Users/UserReadOnlyRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/Users/UserReadOnlyRepository.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Domain.Repositories.Users;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories.Users;
@@ -14,5 +15,10 @@ internal class UserReadOnlyRepository : IUserReadOnlyRepository
     public async Task<bool> ExistActiveUserWithEmail(string email)
     {
         return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
+    }
+
+    public async Task<User?> GetUserByEmail(string email)
+    {
+        return await _dbContext.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
     }
 }
