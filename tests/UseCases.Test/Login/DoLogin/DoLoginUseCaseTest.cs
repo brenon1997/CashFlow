952d89f7a81455f34192aa3b151cc44abf3ec71c
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-15 00:15:35 -0300

    test: add DoLoginUseCaseTest for login use case

---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,71 @@
+﻿using CashFlow.Application.UseCases.Login.DoLogin;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
+using Shouldly;
+
+namespace UseCases.Test.Login.DoLogin;
+public class DoLoginUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+
+        var request = RequestLoginJsonBuilder.Build();
+        request.Email = user.Email;
+
+        var useCase = CreateUseCase(user, request.Password);
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Token.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Error_User_Not_Found()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestLoginJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user, request.Password);
+
+        var act = () => useCase.Execute(request);
+
+        var result = await act.ShouldThrowAsync<InvalidLoginException>();
+
+        result.GetErrors().ShouldHaveSingleItem().ShouldContain(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
+    }
+
+    [Fact]
+    public async Task Error_Password_Not_Match()
+    {
+        var user = UserBuilder.Build();
+
+        var request = RequestLoginJsonBuilder.Build();
+        request.Email = user.Email;
+
+        var useCase = CreateUseCase(user);
+
+        var act = () => useCase.Execute(request);
+
+        var result = await act.ShouldThrowAsync<InvalidLoginException>();
+
+        result.GetErrors().ShouldHaveSingleItem().ShouldContain(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID);
+    }
+
+    private DoLoginUseCase CreateUseCase(CashFlow.Domain.Entities.User user, string? password = null)
+    {
+        var passwordEncripter = new PasswordEncrypterBuilder().Verify(password).Build();
+        var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().GetUserByEmail(user).Build();
+
+        return new DoLoginUseCase(readRepository, passwordEncripter, tokenGenerator);
+    }
+}
