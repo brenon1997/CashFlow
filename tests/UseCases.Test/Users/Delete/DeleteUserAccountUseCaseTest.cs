78e9f37c95960b181d988c4e9d73934338e3526f
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-18 01:10:31 -0300

    test: add DeleteUserAccountUseCaseTest for user account deletion use case

---
+++ tests/UseCases.Test/Users/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Repositories.Users;
+using Shouldly;
+
+namespace UseCases.Test.Users.Delete;
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.ShouldNotThrowAsync();
+    }
+
+    private DeleteUserAccountUseCase CreateUseCase(User user)
+    {
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserAccountUseCase(unitOfWork, repository, loggedUser);
+    }
+}
+
