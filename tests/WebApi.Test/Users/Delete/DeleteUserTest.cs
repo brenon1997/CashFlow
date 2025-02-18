05f71a6718da73775acb9b03ce7b06635f01393b
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-18 01:11:50 -0300

    test: add DeleteUserTest for user deletion integration test

---
+++ tests/WebApi.Test/Users/Delete/DeleteUserTest.cs
@@ -0,0 +1,23 @@
+﻿using Shouldly;
+using System.Net;
+
+namespace WebApi.Test.Users.Delete;
+public class DeleteUserTest : CashFlowClassFixture
+{
+    private const string METHOD = "api/User";
+
+    private readonly string _token;
+
+    public DeleteUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoDelete(METHOD, _token);
+
+        result.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+}
